54be373124f6d07ec2d9822a4d8712734b148436
Gildesio Campos <gildesiocp@outlook.com>
2024-12-14 16:59:54 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -24,5 +24,13 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== TEST 3: seller findByAll =====");
+		list = sellerDao.findByAll();
+
+		for (Seller obj : list) {
+			System.out.println(obj);
+		}
+		
 	}
 }
