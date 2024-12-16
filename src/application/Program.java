a7f982d5664b0a191c5765c21724522dba6e4ce1
Gildesio Campos <gildesiocp@outlook.com>
2024-12-16 12:09:15 -0300

    insert implementation and and update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -32,5 +33,18 @@ public class Program {
 			System.out.println(obj);
 		}
 		
+		
+		System.out.println("\n=== TEST 4: seller insert =====");
+		//Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		Seller newSeller = new Seller(0, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New Id = " + newSeller.getId());
+		
+		
+		System.out.println("\n=== TEST 5: seller update =====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Wayne");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 	}
 }
